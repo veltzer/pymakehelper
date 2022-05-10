074baecb08f30537e18385c105a5e83b20391550
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-10 19:54:55 +0300

    new version

--- pymakehelper/static.py
+++ pymakehelper/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.26"
+VERSION_STR = "0.0.27"
 DESCRIPTION = "pymakehelper eases writing recipes when using the make system"
 APP_NAME = "pymakehelper"
 LOGGER_NAME = "pymakehelper"
