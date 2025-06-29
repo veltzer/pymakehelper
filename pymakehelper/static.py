d764370bf36481524fcb87a70e746464206fd48b
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 09:33:46 +0300

    fixed up deps

--- pymakehelper/static.py
+++
@@ -1,5 +0,0 @@
-""" version which can be consumed from within the module """
-VERSION_STR = "0.0.37"
-DESCRIPTION = "pymakehelper eases writing recipes when using the make system"
-APP_NAME = "pymakehelper"
-LOGGER_NAME = "pymakehelper"
